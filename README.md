4e760bab5abb189f3525e945547d8b40a94188e5
Albin Mamuti <albin-mamuti@hotmail.com>
2021-12-25 23:08:30 +0100

    DiscordBot Day1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# DiscordBot
+# DiscordBot by Albin Mamuti
+
+Check it out, features will be added, but for now it is a fun side project.
--- src/bot.js
+++ src/bot.js
@@ -3,34 +3,52 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ fetchAllMembers: true, intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const PREFIX = "$";
+
+/*
+    This Discord Bot is solely for educational purposes for myself studying NodeJs. It will get some pretty cool features like fetching Data or Products from variouses sources on the internet. But for now it will only be playfull in the Discord Chat
+*/
+
+/* Log everytime the Client/bot starts*/
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in`);
 });
+
+/*Event Listener for when people write in all Chats*/
+/*Mainly used for the Commands prefixed with $ */
 client.on('messageCreate', (message) => {
     const CMD_IMPL = ['kick', 'ban', 'help'];
+    /*Don't let the Bot recurse on it's on messages*/
     if (message.author.bot)
         return;
+    /*check if the message starts with our prefix, for now ignore all other input*/
     if (message.content.startsWith(PREFIX)) {
+        //Log the Command and the Arguments to it.
         const [CMD_NAME, ...args] = message.content
             .trim()
             .substring(1, message.content.length)
             .split(/\s+/);
         console.log(`${message.author.tag} has issued command ${CMD_NAME} with following args ${args}`);
+
+        //Now check which command is typed in and process is
         if (CMD_NAME === 'kick') {
             if (!message.member.permissionsIn(message.channel).has("KICK_MEMBERS"))
-                return message.reply('Din cock isch zchli für das');
+                return message.reply('You dont have Permission to Kick other Members');
             if (!message.member.permissions.has('KICK_MEMBERS'))
-                return message.reply('Du häsch kei Rechte pessant');
+                return message.reply('You dont have Permission to Kick other Members');
+
             if (args.length === 0)
-                return message.reply('Who tf should i kick master, please specify with $kick USER');
+                return message.reply('please specify either USERID or USERTAG, i.e. $kick USERID | USERTAG');
             var regex = /^[0-9]+$/;
-            message.channel.send('As you say mylord');
             let copyArgs0 = args[0];
+            //We check if we have a pure ID or a User Tag, we trim if needed
             if (!args[0].match(regex)) {
                 let n = args[0].length;
                 args[0] = args[0].substring(3, n - 1);
             }
+
             console.log(`searching for ${args[0]}`);
+            //Now we search for the user, if found we kick him, otherwise 
+            //we report back that this User is not a Member of the Discord
             message.guild.members.fetch(args[0]).then((members) => {
                 members.kick()
                     .then((member) => {
@@ -44,6 +62,8 @@ client.on('messageCreate', (message) => {
             })
                 .catch((err) => { return message.reply(`This user ${copyArgs0} is not a member of HSPG`) });
         }
+        //For Ban we do the same as in the Kick command, we just replace it with
+        //the ban command.
         else if (CMD_NAME === 'ban') {
             if (!message.member.permissionsIn(message.channel).has("BAN_MEMBERS"))
                 return message.reply('Din cock isch viel zchli für da');
@@ -51,13 +71,15 @@ client.on('messageCreate', (message) => {
                 return message.reply('Du häsch kei Rechte pessant');
             if (args.length === 0)
                 return message.reply('Who tf should i kick master');
-            message.channel.send('u naughty naugthy, but as you say mylord');
+            //We check if we have a pure ID or a User Tag, we trim if needed
             console.log(`trimming input`);
             var regex = /^[0-9]+$/;
             let copyArgs0 = args[0];
             if (!args[0].match(regex))
                 args[0] = args[0].substring(3, args[0].length - 1);
             console.log(`searching for ${args[0]}`);
+            //Now we search for the user, if found we ban him, otherwise 
+            //we report back that this User is not a Member of the Discord
             message.guild.members.fetch(args[0])
                 .then((member) => {
                     member.ban()
@@ -72,6 +94,8 @@ client.on('messageCreate', (message) => {
                 })
                 .catch((err) => { return message.reply(`This user ${copyArgs0} is not a member of HSPH`) });
         }
+        //Now this will be replaced soon :
+        //TLDR: This should only function as a subsitute and is a little bit of Troll atm. Genereic help response with some swiss-german spice
         else if (CMD_NAME === 'help') {
             let response = "";
             response += `Küss dein auge, vallah endlich eine wo mich brucht.\nAlso Amigo, isch eig voll easy, es git nur die commands:\n`;
@@ -82,8 +106,12 @@ client.on('messageCreate', (message) => {
             response += `Amigo wenn immer no nöd drus chunsch denn häsch ihn gesse vallah billah`;
             message.reply(response);
         }
+        //This command needs work on, check privelge so that only OWNER
+        // is allowed to execute this 
         else if (CMD_NAME === 'kill')
             client.destroy();
+        //Now this is where the troll really starts, but it will be removed once
+        //the bot is more polished
         else {
             const botInfo = '(https://exit.ch/)';
             message.reply(`Brudi bitte mach kein Auge und schrieb $help, wenn das au nöd ane bechunsch, gang uf de link: ${botInfo}`);
